76ce79694840b530b720a63bc744659cb436f91f
neyhartj <neyha001@umn.edu>
2020-03-09 16:53:27 -0500

    remove dos encoding

--- Scripts/PhenotypicModeling/run_phenotype_analysis.sh
+++ Scripts/PhenotypicModeling/run_phenotype_analysis.sh
@@ -9,8 +9,7 @@
 # Change the working directory
 cd /panfs/roc/groups/6/smithkp/neyha001/Projects/BarleyNurseryAnalysis
 
-module load R/3.5.0
-# module load R/3.5.2_mkl
+module load R/3.5.2_mkl
 
 # Met analysis
 Rscript met_mixed_models.R
