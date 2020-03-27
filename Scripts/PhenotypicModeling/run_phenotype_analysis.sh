af132d0dacee4b9e3a0884737b1cc37439ae6709
neyhartj <neyhartje@gmail.com>
2020-03-27 11:27:09 -0500

    Edit the bash script for multiple trait arguments

--- Scripts/PhenotypicModeling/run_phenotype_analysis.sh
+++ Scripts/PhenotypicModeling/run_phenotype_analysis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#PBS -l walltime=36:00:00,mem=250gb,nodes=1:ppn=1
-#PBS -N met_mixed_model
+#PBS -l walltime=48:00:00,mem=250gb,nodes=1:ppn=1
+#PBS -N met_mixed_model_GrainProtein
+# #PBS -N met_mixed_model_MaltExtract
+# #PBS -N met_mixed_model_GrainYield
+# #PBS -N met_mixed_model_HeadingDate
+# #PBS -N met_mixed_model_PlantHeight
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
@@ -12,6 +16,21 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Projects/BarleyNurseryAnalysis/Scripts/P
 
 module load R/3.5.2_mkl
 
-# Met analysis
-Rscript met_mixed_models.R
+
+## Run the R script
+# GrainProtein
+Rscript met_mixed_models.R GrainProtein
+
+# MaltExtract
+# Rscript met_mixed_models.R MaltExtract
+
+# GrainYield
+# Rscript met_mixed_models.R GrainYield
+
+# HeadingDate
+# Rscript met_mixed_models.R HeadingDate
+
+# PlantHeight
+# Rscript met_mixed_models.R PlantHeight
+
 
